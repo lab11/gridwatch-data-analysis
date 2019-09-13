628d2d82e245d0210349781c60ed78795eac255a
Joshua Adkuins <adkins@berkeley.edu>
2019-09-13 15:08:06 -0700

    we just shouldn't mess with the path. do it all locally

--- ipynb-setup.sh
+++ ipynb-setup.sh
@@ -1,8 +1,6 @@
-echo 'export PATH="$PATH:'$(pwd)/ipython-cleaner'"' >> ~/.bashrc
-source ~/.bashrc
 mkdir -p ~/.config
 mkdir -p ~/.config/git
 touch ~/.config/git/attributes
 echo '*.ipynb  filter=clean_ipynb' >> ~/.config/git/attributes
-git config filter.clean_ipynb.clean ipynb_drop_output.py
+git config filter.clean_ipynb.clean ./ipython-cleaner/ipynb_drop_output.py
 git config filter.clean_ipynb.smudge cat
